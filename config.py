664bc9ba3609c6d905db39d2b6caf9fde0407297
stenpav109-lab <stenpav109@gmail.com>
2026-02-20 22:03:47 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = os.getenv("ADMIN_ID", 0)
 
 # Настройки
 MIN_METRAGE = 30
